44be9b1d3f8d9bc4a99b5dbd33bb37949fec7fc7
Brendan Jackman <bhenryj0117@gmail.com>
2019-11-24 17:26:17 +0100

    Use prepared statements
    argh
    https://stackoverflow.com/questions/32300132/why-cant-i-store-a-value-and-a-reference-to-that-value-in-the-same-struct

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 use std::time::{ SystemTime, UNIX_EPOCH, Duration };
 use std::error::Error;
 use uom::si::{ f64::ThermodynamicTemperature, thermodynamic_temperature::degree_celsius };
-use rusqlite::{ Connection, params, };
+use rusqlite::{ Connection, Statement, params };
 
 #[derive(Debug)]
 pub struct TempRecord {
@@ -9,14 +9,13 @@ pub struct TempRecord {
     pub temp: ThermodynamicTemperature,
 }
 
-pub struct TempDb {
-    conn: Connection,
+pub struct TempDb<'a> {
+    insert_stmt: Statement<'a>,
+    query_stmt: Statement<'a>,
 }
 
-impl TempDb {
-    pub fn new() -> Result<TempDb, Box<dyn Error>> {
-        let conn = Connection::open_in_memory()?;
-
+impl<'a> TempDb<'a> {
+    pub fn new(conn: &'a Connection) -> Result<TempDb<'a>, Box<dyn Error>> {
         conn.execute(
             "CREATE TABLE temperature (
                  id          INTEGER PRIMARY KEY,
@@ -24,29 +23,29 @@ impl TempDb {
                  temp_c      REAL)",
             params![])?;
 
+        let insert_stmt = conn.prepare(
+            "INSERT INTO temperature (timestamp_s, temp_c)
+                    VALUES ((:timestamp_s), (:temp_c))",)?;
+        let query_stmt = conn.prepare(
+            "SELECT timestamp_s, temp_c FROM temperature")?;
+
         Ok(TempDb {
-            conn
+            insert_stmt,
+            query_stmt,
         })
     }
 
     pub fn insert(&mut self) -> Result<(), Box<dyn Error>> {
         let now_s = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
-        // TODO use prepared satetment
-        self.conn.execute(
-            "INSERT INTO temperature (timestamp_s, temp_c)
-                 VALUES (?1, ?2)",
-            // TODO how to get temp as int?
-            params![now_s as i64, 30i32])?;
+        self.insert_stmt.execute_named(&[(":timestamp_s", &(now_s as i64)),
+                                         (":temp_c", &30)])?;
         Ok(())
     }
 
     // TODO hide MappedRows from caller?
-    pub fn iter(&self) -> Result<(), Box<dyn Error>> {
-        // TODO use prepared satetment
-        let mut statement = self.conn.prepare(
-            "SELECT timestamp_s, temp_c FROM temperature")?;
+    pub fn iter(&mut self) -> Result<(), Box<dyn Error>> {
         // TODO figure out how to return an iterator
-        let row_iter = statement.query_map(params![], |row| {
+        let row_iter = self.query_stmt.query_map(params![], |row| {
             let timestamp_s: i64 = row.get(0)?; // s64?
             Ok(TempRecord {
                 time: UNIX_EPOCH + Duration::new(timestamp_s as u64, 0),
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,16 @@ mod db;
 
 use db::TempDb;
 use std::error::Error;
+use rusqlite::Connection;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut db = TempDb::new()?;
+    // TODO this sucks.
+    // See the probem described here:
+    // https://stackoverflow.com/questions/48117710/return-a-reference-together-with-the-referenced-object-in-rust
+    // Having the Conncetion allocated in the caller gets around hte issue, but
+    // I'm not happy with it.
+    let conn = Connection::open_in_memory()?;
+    let mut db = TempDb::new(&conn)?;
     db.insert()?;
     db.iter()?;
 
