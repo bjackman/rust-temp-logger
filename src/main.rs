2e7d7c9aef4c5cdcc154b2f8392a9be4a79ba4bb
Brendan Jackman <bhenryj0117@gmail.com>
2019-12-15 22:27:11 +0100

    Add a thread to poll the sensor

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use db::{ Temp, degree_celsius, TempDb };
 use std::error::Error;
 use rusqlite::Connection;
 use gnuplot::{ Figure };
+use std::sync::{ Arc, Mutex, Condvar };
+use std::thread;
+use std::thread::sleep;
 
 #[macro_use]
 extern crate clap;
@@ -24,12 +27,36 @@ fn insert_fake_data(db: &mut TempDb) {
     }
 }
 
+// Poll the sensor until the bool guarded by the Mutex+Condvar is set to true
+fn do_poll(mutex_cond: Arc<(Mutex<bool>, Condvar)>) {
+    let (mutex, cond) = &*mutex_cond;
+    let mut done = mutex.lock().unwrap();
+    loop {
+        sensor::poll();
+
+        let result = cond.wait_timeout(done, Duration::from_millis(1000)).unwrap();
+        // Can't do re-structuring assignment without "let" hence use of
+        // intermediate variable here
+        done = result.0;
+        if *done {
+            break;
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Start by parsing arguments
     let matches = clap_app!(app =>
         (@arg DB_PATH: --db +takes_value "Path to temperature DB. Created if nonexistent")
     ).get_matches();
     let db_path = matches.value_of("db").unwrap_or("/tmp/temp-logger.sqlite");
+
+    // Spin up a thread to poll the sensor
+    let poll_killer = Arc::new((Mutex::new(false), Condvar::new()));
+    let poll_killer2 = poll_killer.clone();
+    thread::spawn(move|| {
+        do_poll(poll_killer2);
+    });
 
     // TODO this sucks.
     // See the probem described here:
@@ -56,6 +83,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     fg.show().unwrap();
 
     web::serve();
+
+    // Gracefully terminate the sensor polling thread
+    sleep(Duration::from_millis(15));
+    let (lock, cond) = &*poll_killer;
+    {
+        let mut done = lock.lock().unwrap();
+        *done = true;
+        cond.notify_all();
+    }
 
     Ok(())
 }
